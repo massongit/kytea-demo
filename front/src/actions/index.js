1e176507de12d13c2f893b596e2e4502fc214f00
massongit <suzukimasaya428@gmail.com>
2018-07-25 12:49:36 +0900

    Simplified process

--- front/src/actions/index.js
+++ front/src/actions/index.js
@@ -2,13 +2,32 @@ import * as types from "./types"
 import {createAction} from "redux-actions"
 import {intl} from "../intl"
 
-const convertTag = (pos) => {
-    if (pos === "(Unknown)") {
+/**
+ * タグを変換する
+ * @param tag タグ
+ * @returns {string} タグ
+ */
+const convertTag = (tag) => {
+    if (tag === "(Unknown)") {
         return intl.formatMessage({
             id: "unknown"
         })
     } else {
-        return pos
+        return tag
+    }
+}
+
+/**
+ * 与えられたArrayに対し、map操作を行えそうなら行い、行えないようであれば空のArrayを返す
+ * @param l {array}
+ * @param f {function} 操作
+ * @returns {array}
+ */
+const mapOrEmptyArray = (l, f) => {
+    if (l) {
+        return l.map(f)
+    } else {
+        return []
     }
 }
 
@@ -19,20 +38,20 @@ const convertTag = (pos) => {
 export const showSentence = createAction(types.SHOW_SENTENCE, parameter => {
     // KyTeaによる解析結果の表示Action
     const action = {
-        words: parameter.words ? parameter.words.map(w => {
+        words: mapOrEmptyArray(parameter.words, w => {
             if (w) {
                 return {
                     word: w.word,
-                    pronunciation: w.pronunciation ? w.pronunciation.map(p => {
+                    pronunciation: mapOrEmptyArray(w.pronunciation, p => {
                         p.pronunciation = convertTag(p.pronunciation)
                         return p
-                    }) : [],
+                    }),
                     pos: convertTag(w.pos)
                 }
             } else {
                 return w
             }
-        }) : []
+        })
     }
 
     // 空白を除去したsentenceをActionにセット
