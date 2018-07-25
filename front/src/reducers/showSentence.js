33d523ef1871b109e3547ccd7cefec7a2125591d
massongit <suzukimasaya428@gmail.com>
2018-07-25 09:33:10 +0900

    Simplified process

--- front/src/reducers/showSentence.js
+++ front/src/reducers/showSentence.js
@@ -10,18 +10,16 @@ export default handleActions(
         [types.SHOW_SENTENCE]: (state, action) => {
             if (action.payload.words) {
                 // sentence (検査用)
-                let sentence = ""
-
-                for (const w of action.payload.words) {
+                const sentence = action.payload.words.map(w => {
                     if (w && w.word) { // wordsから単語を取り出し、sentence (検査用) に追加
-                        sentence += w.word
+                        return w.word
                     } else { // words内にデータの欠損が存在する場合、Stateを更新しない
-                        return state
+                        return undefined
                     }
-                }
+                })
 
                 // wordsから取り出した単語とsentenceが一致するとき、Stateを更新
-                if (action.payload.sentence === sentence.replace(/\s/g, "")) {
+                if (!sentence.includes(undefined) && action.payload.sentence === sentence.join("").replace(/\s/g, "")) {
                     return {
                         sentence: action.payload.sentence,
                         words: action.payload.words
