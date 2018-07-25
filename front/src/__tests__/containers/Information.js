7725b510922738e4d3f5bc4f2b914c31f82e60b0
massongit <suzukimasaya428@gmail.com>
2018-07-25 14:51:16 +0900

    Separate process to function

--- front/src/__tests__/containers/Information.js
+++ front/src/__tests__/containers/Information.js
@@ -7,50 +7,50 @@ import {createStore} from "redux"
 import {showPOSAndPronunciation, showSentence} from "../../actions"
 import {pos, pronunciation, showPOSAndPronunciationState_, showSentenceParameter, word} from "../reducers"
 
-let store
+/**
+ * Storeを作成する
+ * @returns {store} Store
+ */
+const makeStore = () => {
+    const store = createStore(rootReducer)
+    store.dispatch(showSentence(deepcopy(showSentenceParameter)))
+    store.dispatch(showPOSAndPronunciation(showPOSAndPronunciationState_))
+    return store
+}
+
+let informationComponent
 
 loadTranslation("./src/translations/ja.json")
 
-describe("containers/Information", () => {
-    beforeEach(() => {
-        store = createStore(rootReducer)
-        store.dispatch(showSentence(deepcopy(showSentenceParameter)))
-        store.dispatch(showPOSAndPronunciation(showPOSAndPronunciationState_))
-    })
-
+describe("containers/Information/snapshot", () => {
     it("初期状態からshowSentence, showPOSAndPronunciationとStateが遷移した際に、Componentが正しく配置されている", () => {
         const informationComponent = shallowWithIntl(
             <Information
-                store={store}
+                store={makeStore()}
             />
         ).dive()
         expect(informationComponent).toMatchSnapshot()
     })
+})
 
-    it("初期状態からshowSentence, showPOSAndPronunciationとStateが遷移した際に、子要素にposが含まれる", () => {
-        const informationComponent = mountWithIntl(
+describe("containers/Information/other", () => {
+    beforeEach(() => {
+        informationComponent = mountWithIntl(
             <Information
-                store={store}
+                store={makeStore()}
             />
         )
+    })
+
+    it("初期状態からshowSentence, showPOSAndPronunciationとStateが遷移した際に、子要素にposが含まれる", () => {
         expect(informationComponent.contains(pos)).toBeTruthy()
     })
 
     it("初期状態からshowSentence, showPOSAndPronunciationとStateが遷移した際に、子要素にwordが含まれる", () => {
-        const informationComponent = mountWithIntl(
-            <Information
-                store={store}
-            />
-        )
         expect(informationComponent.contains(word)).toBeTruthy()
     })
 
     it("初期状態からshowSentence, showPOSAndPronunciationとStateが遷移した際に、子要素にpronunciationが含まれる", () => {
-        const informationComponent = mountWithIntl(
-            <Information
-                store={store}
-            />
-        )
         for (const w of pronunciation) {
             expect(informationComponent.contains(w.pronunciation)).toBeTruthy()
         }
