c890c9f834c45109b5c8166afcd5e5a40b443f48
massongit <suzukimasaya428@gmail.com>
2018-07-25 09:39:51 +0900

    Separate process to function

--- front/src/reducers/showSentence.js
+++ front/src/reducers/showSentence.js
@@ -2,6 +2,21 @@ import * as types from "../actions/types"
 import {handleActions} from "redux-actions"
 
 /**
+ * wordsから単語を取り出し、リストを作成する
+ * @param words words
+ * @returns {[string]}
+ */
+const makeWordList = (words) => (
+    words.map(w => {
+        if (w && w.word) { // 正常なデータが存在するとき
+            return w.word
+        } else { // データの欠損が存在するとき
+            return undefined
+        }
+    })
+)
+
+/**
  * KyTeaによる解析結果の表示ActionのReducer
  */
 export default handleActions(
@@ -9,17 +24,11 @@ export default handleActions(
         // KyTeaによる解析結果の表示Actionのとき
         [types.SHOW_SENTENCE]: (state, action) => {
             if (action.payload.words) {
-                // sentence (検査用)
-                const sentence = action.payload.words.map(w => {
-                    if (w && w.word) { // wordsから単語を取り出し、sentence (検査用) に追加
-                        return w.word
-                    } else { // words内にデータの欠損が存在する場合、Stateを更新しない
-                        return undefined
-                    }
-                })
+                // wordsから取り出した単語のリスト
+                const wordList = makeWordList(action.payload.words)
 
-                // wordsから取り出した単語とsentenceが一致するとき、Stateを更新
-                if (!sentence.includes(undefined) && action.payload.sentence === sentence.join("").replace(/\s/g, "")) {
+                // wordsに欠損がなく、wordsから取り出した単語とsentenceが一致するとき、Stateを更新
+                if (!wordList.includes(undefined) && action.payload.sentence === wordList.join("").replace(/\s/g, "")) {
                     return {
                         sentence: action.payload.sentence,
                         words: action.payload.words
