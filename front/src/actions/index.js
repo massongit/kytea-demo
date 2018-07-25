6fc934a68cd7529c4cacc3d6ecbe7dc792ae068b
massongit <suzukimasaya428@gmail.com>
2018-07-25 12:35:59 +0900

    Simplified process

--- front/src/actions/index.js
+++ front/src/actions/index.js
@@ -2,6 +2,16 @@ import * as types from "./types"
 import {createAction} from "redux-actions"
 import {intl} from "../intl"
 
+const convertTag = (pos) => {
+    if (pos === "(Unknown)") {
+        return intl.formatMessage({
+            id: "unknown"
+        })
+    } else {
+        return pos
+    }
+}
+
 /**
  * KyTeaによる解析結果の表示ActionのActionCreator
  * @type {function}
@@ -9,49 +19,25 @@ import {intl} from "../intl"
 export const showSentence = createAction(types.SHOW_SENTENCE, parameter => {
     // KyTeaによる解析結果の表示Action
     const action = {
-        words: []
+        words: parameter.words ? parameter.words.map(w => {
+            if (w) {
+                return {
+                    word: w.word,
+                    pronunciation: w.pronunciation ? w.pronunciation.map(p => {
+                        p.pronunciation = convertTag(p.pronunciation)
+                        return p
+                    }) : [],
+                    pos: convertTag(w.pos)
+                }
+            } else {
+                return w
+            }
+        }) : []
     }
 
     // 空白を除去したsentenceをActionにセット
     if (parameter.sentence) {
         action.sentence = parameter.sentence.replace(/\s/g, "")
-    }
-
-    if (parameter.words) {
-        // 予測不能であった場合のタグ
-        const unknownTag = "(Unknown)"
-
-        for (const word of parameter.words) {
-            if (word) {
-                // Actionのword
-                const word_ = {
-                    word: word.word,
-                    pronunciation: []
-                }
-
-                if (word.pos === unknownTag) {
-                    word.pos = intl.formatMessage({
-                        id: "unknown"
-                    })
-                } else {
-                    word_.pos = word.pos
-                }
-
-                for (let i = 0; word.pronunciation && i < word.pronunciation.length; i++) {
-                    if (word.pronunciation[i].pronunciation === unknownTag) {
-                        word.pronunciation[i].pronunciation = intl.formatMessage({
-                            id: "unknown"
-                        })
-                    }
-
-                    word_.pronunciation.push(word.pronunciation[i])
-                }
-
-                action.words.push(word_)
-            } else {
-                action.words.push(word)
-            }
-        }
     }
 
     return action
