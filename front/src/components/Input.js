a141e3da0c336d3f684a8e1282e45d872cb9f03e
massongit <suzukimasaya428@gmail.com>
2018-07-26 10:20:37 +0900

    Separate onSubmit event

--- front/src/components/Input.js
+++ front/src/components/Input.js
@@ -15,44 +15,51 @@ class Input extends React.Component {
         intl: intlShape.isRequired
     }
 
+    constructor(props) {
+        super(props)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    async onSubmit(ev) {
+        // サーバーへのSubmitが行われないようにする
+        ev.preventDefault()
+
+        this.input.value = this.input.value.trim()
+
+        // 以前の入力文とは異なる文章が入力されたとき、KyTeaによる解析を行い、KyTeaによる解析結果の表示Actionをdispatch
+        if (0 < this.input.value.length && this.input.value.replace(/\s/g, "") !== this.props.sentence) {
+            try {
+                this.props.dispatch(showSentence({
+                    sentence: this.input.value,
+                    words: await (await fetch(
+                        "/kytea",
+                        {
+                            method: "POST",
+                            body: this.input.value
+                        }
+                    ).then(res => {
+                        if (res.ok) {
+                            return res
+                        } else {
+                            throw new Error(res.statusText)
+                        }
+                    })).json()
+                }))
+            } catch (er) {
+                alert(this.props.intl.formatMessage({
+                    id: "errorMessage.predict"
+                },
+                {
+                    message: er.message
+                }))
+            }
+        }
+    }
+
     render() {
         return (
             <Form
-                onSubmit={async ev => {
-                    // サーバーへのSubmitが行われないようにする
-                    ev.preventDefault()
-
-                    this.input.value = this.input.value.trim()
-
-                    // 以前の入力文とは異なる文章が入力されたとき、KyTeaによる解析を行い、KyTeaによる解析結果の表示Actionをdispatch
-                    if (0 < this.input.value.length && this.input.value.replace(/\s/g, "") !== this.props.sentence) {
-                        try {
-                            this.props.dispatch(showSentence({
-                                sentence: this.input.value,
-                                words: await (await fetch(
-                                    "/kytea",
-                                    {
-                                        method: "POST",
-                                        body: this.input.value
-                                    }
-                                ).then(res => {
-                                    if (res.ok) {
-                                        return res
-                                    } else {
-                                        throw new Error(res.statusText)
-                                    }
-                                })).json()
-                            }))
-                        } catch (er) {
-                            alert(this.props.intl.formatMessage({
-                                id: "errorMessage.predict"
-                            },
-                            {
-                                message: er.message
-                            }))
-                        }
-                    }
-                }}
+                onSubmit={this.onSubmit}
             >
                 <InputGroup>
                     <FormControl
